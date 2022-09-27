8416ca400aa400d81343f115339653f85a896192
Elliot-59 <elliot.59@outlook.com>
2022-09-27 11:37:23 +0600

    implemented auto delete media while axios.put

--- Status_App/urls.py
+++ Status_App/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', StatusListCreateApiView.as_view()),
-    path('<id>/', StatusRetriveUpdateDestroyApiView.as_view())
+    path('list', StatusListCreateApiView.as_view()),
+    path('retrieve/<id>/', StatusRetriveUpdateDestroyApiView.as_view())
 
 ]
--- main/settings.py
+++ main/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'django_cleanup',
     'Status_App',
 ]
 
